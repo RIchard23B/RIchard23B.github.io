21027c37703085ecc3033bccbf4f8fc2f529caa5
Richard Briones <rbrionesq17_1@unc.edu.pe>
2021-09-05 22:45:45 -0500

    Boton Extraer

--- js/lab05.js
+++ js/lab05.js
@@ -1,9 +1,92 @@
 
+(function (){
+    
+    let boton = document.getElementById("extraer")
+    
 
-//COLOR 
+    boton.addEventListener("click", function(){
+
+    //=============== Separar APELLIDOS Y NOMBRES ====================//
+    //===============================================================//
+
+        const apeNom = document.getElementById('nombresComplet');
+        let nombreCompleto = apeNom.value;
+
+        /** con split elimino de acuerdo a la exprexion regular en este
+        caso eliminan los espacios y me devuelve un array */
+
+        let newN = nombreCompleto.split(/\s+/);
+        /** agrego a cada campo su valor*/
+
+        const apePaterno = document.getElementById('apePaterno');
+        apePaterno.value = newN[0];
+        const apeMaterno = document.getElementById('apeMaterno');
+        apeMaterno.value = newN[1];
+
+        const nombres = document.getElementById('nombre');
+        for (let i = 2; i < newN.length; i++) {
+            nombres.value += " "+newN[i];
+        }
+        nombres = this.focus;
+
+    //===============LONGITUD DE ELEMENTOS====================//
+    //=======================================================//
+        const longitud = document.getElementById('longApe');
+        longitud.value = apeMaterno.value.length+apeMaterno.value.length;
+
+
+    //===============CALCULAR EDAD====================//
+    //===============================================//
+
+    let edades = document.getElementById("anio").value;
+    let mes = document.getElementById("mes").value;
+
+    var hoy = new Date();
+    var cumpleanos = new Date(edades);
+    var cumple = new Date(mes)
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumple.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+
+    }
+    document.getElementById("edad").value = edad;
+
+
+    });
+
+})();
+
+
+//=============== FECHA EN LETRAS ====================//
+//===============================================//
+(function(){ 
+
+    let boton = document.getElementById("extraer");
+
+    boton.addEventListener("click",function(){
+        
+        let mes = document.getElementById("mes").value;
+
+        let nombreMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+        let d = new Date(mes);
+        document.getElementById("meses").value = (" " + nombreMeses[d.getMonth()]); 
+        
+    });
+
+})();
+
+
+
+//=============== COLOR ====================//
+//===============================================//
 (function() {
 
     let boton = document.getElementById("colorear");
+    
     let titulo = document.getElementById("titulo");
     let titulos = document.getElementById("titulo-seg");
     let tituloss = document.getElementById("titulo-ter");
